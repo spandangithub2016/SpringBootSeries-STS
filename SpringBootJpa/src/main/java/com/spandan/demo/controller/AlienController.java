2536a6a2c4cf96faa9bd3e233a730963bd13fdf0
Spandan Majumder <spandance2016@gmail.com>
2019-01-05 12:53:09 +0530

    Commit changes for: 1. Use of @PutMapping 2. Use of @PostMapping 3. Use of @GetMapping 3. Use of @DeleteMapping
    Note:- we have used JpaRepository here

--- SpringBootJpa/src/main/java/com/spandan/demo/controller/AlienController.java
+++ SpringBootJpa/src/main/java/com/spandan/demo/controller/AlienController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,7 +19,7 @@ import com.spandan.demo.dao.AlienRepo;
 import com.spandan.demo.model.Alien;
 
 @RestController
-@ResponseBody
+//@ResponseBody
 public class AlienController {
 	
 	@Autowired
@@ -26,12 +30,12 @@ public class AlienController {
 		return "home.jsp";
 	}
 
-	@RequestMapping("/addAlien")
-	public String addAlien(Alien alien) {
-		
-		repo.save(alien);
-		return "home.jsp";
-	}
+//	@RequestMapping("/addAlien")
+//	public String addAlien(Alien alien) {
+//		
+//		repo.save(alien);
+//		return "home.jsp";
+//	}
 	
 	@RequestMapping("/getAlien")
 	public ModelAndView addAlien(@RequestParam int aid) {
@@ -50,11 +54,19 @@ public class AlienController {
 		return modelAndView;
 	}
 	
+	@PostMapping("/alien")
+	public Alien addAlien(@RequestBody Alien alien) {
+		
+		repo.save(alien);
+		return alien;
+	}
+	
 	/**
-	 *  Jackson-core is responsible to convert java onject to JSON 
+	 *  Jackson-core is responsible to convert java object to JSON 
+	 *  Jackson-dataformat-xml to convert java object to XML
 	 *  */
 	
-	@RequestMapping("/aliens")
+	@GetMapping(path = "/aliens")
 	public List<Alien> getAllAlien() {
 		return repo.findAll();
 
@@ -62,11 +74,28 @@ public class AlienController {
 	}
 	
 	@RequestMapping("/alien/{aid}")
-	
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
 		return repo.findById(aid);
 
 	}
 	
+	/**
+	 *  update example is restricted to accept only json type of data if we use "consumes"
+	 */
+	
+	@PutMapping(path = "/alien", consumes = "application/json")
+	public Alien updateAliedn(@RequestBody Alien alien)
+	{
+		repo.save(alien);
+		return alien;
+	}
+	
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid) {
+		
+		Alien a = repo.getOne(aid);
+		repo.delete(a);
+		return "Deleted";
+	}
 	
 }
