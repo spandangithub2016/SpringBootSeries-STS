3b478c0ad03df749ffa8e80356b53cbc52213441
Spandan Majumder <spandan.majumder@sysbiz.com>
2019-01-09 20:56:19 +0530

    added another example of microservice with spring boot

---
+++ spring-boot-microservice-forex-service/src/main/java/com/spandan/microservice/example/forex/SpringBootMicroserviceForexServiceApplication.java
@@ -0,0 +1,14 @@
+package com.spandan.microservice.example.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
+	}
+
+}
+
